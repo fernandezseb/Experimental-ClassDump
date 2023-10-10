97e68459b2bb766d7f452ddef40cec007d8340a2
Sebastiaan Fernandez <fernandezsebastiaan@gmail.com>
2023-10-10 14:53:55 +0200

    Improve instruction printing

--- src/Printer/ClassPrinter.cpp
+++ src/Printer/ClassPrinter.cpp
@@ -317,7 +317,7 @@ void ClassPrinter::printCode(const AttributeCode* code, const MethodInfo* method
 ClassPrinter::ClassPrinter()
 {
 	instructions.push_back({nop, 0, "nop"});
-	instructions.push_back({ bipush, 1, "bipush", SignedBytePrinter});
+	instructions.push_back({ bipush, 1, "bipush", SignedBytePrinter}); // SIGN EXTENDED
 	instructions.push_back({ istore_0, 0, "istore_0"});
 	instructions.push_back({ istore_1, 0, "istore_1"});
 	instructions.push_back({ istore_2, 0, "istore_2"});
@@ -329,18 +329,16 @@ ClassPrinter::ClassPrinter()
 	instructions.push_back({ aaload, 0, "aaload"});
 	instructions.push_back({ aastore, 0, "aastore"});
 	instructions.push_back({ aconst_null, 0, "aconst_null" });
-	instructions.push_back({ aload, 1, "aload", SignedBytePrinter });
+	instructions.push_back({ aload, 1, "aload", SignedBytePrinter }); // UNSIGNED BYTE INDEX TO LOCAL VARIABLE
 	instructions.push_back({ aload_0, 0, "aload_0"});
 	instructions.push_back({ aload_1, 0, "aload_1"});
 	instructions.push_back({ aload_2, 0, "aload_2" });
 	instructions.push_back({ aload_3, 0, "aload_3" });
-	instructions.push_back({ anewarray, 0, "anewarray", ShortIndices }); // TODO: Check
+	instructions.push_back({ anewarray, 2, "anewarray", ShortIndices });
 	instructions.push_back({ areturn, 0, "areturn"});
 	instructions.push_back({ aload_1, 0, "aload_1" });
 	instructions.push_back({ arraylength, 0, "arraylength" });
-	instructions.push_back({ arraylength, 0, "arraylength" });
-	instructions.push_back({ astore, 1, "astore", SignedBytePrinter });
-	instructions.push_back({ arraylength, 0, "arraylength" });
+	instructions.push_back({ astore, 1, "astore", SignedBytePrinter }); // UNSIGNED BYTE LOCAL VARIABLE
 	instructions.push_back({ astore_0, 0, "astore_0" });
 	instructions.push_back({ astore_1, 0, "astore_1"});
 	instructions.push_back({ astore_2, 0, "astore_2" });
@@ -363,10 +361,12 @@ ClassPrinter::ClassPrinter()
 	instructions.push_back({ dadd, 0, "dadd" });
 	instructions.push_back({ daload, 0, "daload" });
 	instructions.push_back({ dastore, 0, "dastore" });
+	instructions.push_back({ dcmpl, 0, "dcmpl" });
+	instructions.push_back({ dcmpg, 0, "dcmpg" });
 	instructions.push_back({ dconst_0, 0, "dconst_0" });
 	instructions.push_back({ dconst_1, 0, "dconst_1" });
 	instructions.push_back({ ddiv, 0, "ddiv" });
-	instructions.push_back({ dload, 0, "dload" });
+	instructions.push_back({ dload, 1, "dload" }); // INDEX AND INDEX+1 IS UNSIGNED BYTE
 	instructions.push_back({ dload_0, 0, "dload_0" });
 	instructions.push_back({ dload_1, 0, "dload_1" });
 	instructions.push_back({ dload_2, 0, "dload_2" });
@@ -375,7 +375,7 @@ ClassPrinter::ClassPrinter()
 	instructions.push_back({ dneg, 0, "dneg" });
 	instructions.push_back({ drem, 0, "drem" });
 	instructions.push_back({ dreturn, 0, "dreturn" });
-	instructions.push_back({ dstore, 1, "dstore", SignedBytePrinter });
+	instructions.push_back({ dstore, 1, "dstore", SignedBytePrinter }); // UNSIGNED BYTE INDEX AND INDEX+1
 	instructions.push_back({ dstore_0, 0, "dstore_0" });
 	instructions.push_back({ dstore_1, 0, "dstore_1" });
 	instructions.push_back({ dstore_2, 0, "dstore_2" });
@@ -393,11 +393,13 @@ ClassPrinter::ClassPrinter()
 	instructions.push_back({ fadd, 0, "fadd" });
 	instructions.push_back({ faload, 0, "faload" });
 	instructions.push_back({ fastore, 0, "fastore" });
+	instructions.push_back({ fcmpl, 0, "fcmpl" });
+	instructions.push_back({ fcmpg, 0, "fcmpg" });
 	instructions.push_back({ fconst_0, 0, "fconst_0" });
 	instructions.push_back({ fconst_1, 0, "fconst_1" });
 	instructions.push_back({ fconst_2, 0, "fconst_2" });
 	instructions.push_back({ fdiv, 0, "fdiv" });
-	instructions.push_back({ fload, 1, "fload", SignedBytePrinter });
+	instructions.push_back({ fload, 1, "fload", SignedBytePrinter }); // UNSIGNED BYTE: LOCAL VARIABLE
 	instructions.push_back({ fload_0, 0, "fload_0" });
 	instructions.push_back({ fload_1, 0, "fload_1" });
 	instructions.push_back({ fload_2, 0, "fload_2" });
@@ -406,7 +408,7 @@ ClassPrinter::ClassPrinter()
 	instructions.push_back({ fneg, 0, "fneg" });
 	instructions.push_back({ frem, 0, "frem" });
 	instructions.push_back({ freturn, 0, "freturn" });
-	instructions.push_back({ fstore, 1, "fstore", SignedBytePrinter });
+	instructions.push_back({ fstore, 1, "fstore", SignedBytePrinter }); // UNSIGNED BYTE LOCAL VARIABLE
 	instructions.push_back({ fstore_0, 0, "fstore_0" });
 	instructions.push_back({ fstore_1, 0, "fstore_1" });
 	instructions.push_back({ fstore_2, 0, "fstore_2" });
@@ -414,8 +416,8 @@ ClassPrinter::ClassPrinter()
 	instructions.push_back({ fsub, 0, "fsub" });
 	instructions.push_back({ getfield, 2, "getfield", ShortIndices });
 	instructions.push_back({ getstatic, 2, "getstatic", ShortIndices });
-	instructions.push_back({ i_goto, 2, "goto", ShortIndices });
-	instructions.push_back({ goto_w, 4, "goto_w", ShortIndices }); // TODO: needs to be a wider printer
+	instructions.push_back({ i_goto, 2, "goto", ShortIndices }); // SIGNED BRANCHOFFSET
+	instructions.push_back({ goto_w, 4, "goto_w", ShortIndices }); // 32 bit SIGNED
 	instructions.push_back({ i2b, 0, "i2b" });
 	instructions.push_back({ i2c, 0, "i2c" });
 	instructions.push_back({ i2d, 0, "i2d" });
@@ -434,8 +436,24 @@ ClassPrinter::ClassPrinter()
 	instructions.push_back({ iconst_4, 0, "iconst_4" });
 	instructions.push_back({ iconst_5, 0, "iconst_5" });
 	instructions.push_back({ idiv, 0, "idiv" });
-	instructions.push_back({ iinc, 2, "iinc", SignedBytePrinter });
-	instructions.push_back({ iload, 1, "iload", SignedBytePrinter });
+	instructions.push_back({ if_acmpeq, 2, "if_acmpeq", SignedBytePrinter }); // SIGNED 16 bit offset 
+	instructions.push_back({ if_acmpne, 2, "if_acmpne",SignedBytePrinter }); // SIGNED  16 bit offset
+	instructions.push_back({ if_icmpeq, 2, "if_icmpeq",SignedBytePrinter }); // SIGNED  16 bit offset
+	instructions.push_back({ if_icmpne, 2, "if_icmpne",SignedBytePrinter }); // SIGNED  16 bit offset
+	instructions.push_back({ if_icmplt, 2, "if_icmplt",SignedBytePrinter }); // SIGNED  16 bit offset
+	instructions.push_back({ if_icmpge, 2, "if_icmpge",SignedBytePrinter }); // SIGNED  16 bit offset
+	instructions.push_back({ if_icmpgt, 2, "if_icmpgt",SignedBytePrinter }); // SIGNED  16 bit offset
+	instructions.push_back({ if_icmple, 2, "if_icmple",SignedBytePrinter }); // SIGNED  16 bit offset
+	instructions.push_back({ ifeq, 2, "ifeq",SignedBytePrinter }); // SIGNED  16 bit offset
+	instructions.push_back({ ifne, 2, "ifne",SignedBytePrinter }); // SIGNED  16 bit offset
+	instructions.push_back({ iflt, 2, "iflt",SignedBytePrinter }); // SIGNED  16 bit offset
+	instructions.push_back({ ifge, 2, "ifge",SignedBytePrinter }); // SIGNED  16 bit offset
+	instructions.push_back({ ifgt, 2, "ifgt",SignedBytePrinter }); // SIGNED  16 bit offset
+	instructions.push_back({ ifle, 2, "ifle",SignedBytePrinter }); // SIGNED  16 bit offset
+	instructions.push_back({ ifnonnull, 2, "ifnonnull",SignedBytePrinter }); // SIGNED  16 bit offset
+	instructions.push_back({ ifnull, 2, "ifnull",SignedBytePrinter }); // SIGNED  16 bit offset
+	instructions.push_back({ iinc, 2, "iinc", SignedBytePrinter }); // 1 UNSIGNED BYTE LOCAL VARIABLE AND SIGNED BYTE
+	instructions.push_back({ iload, 1, "iload", SignedBytePrinter }); // UNSIGNED BYTE LOCAL VARIABLE
 	instructions.push_back({ iload_0, 0, "iload_0" });
 	instructions.push_back({ iload_1, 0, "iload_1" });
 	instructions.push_back({ iload_2, 0, "iload_2" });
@@ -448,15 +466,19 @@ ClassPrinter::ClassPrinter()
 	instructions.push_back({ invokespecial, 2, "invokespecial", ShortIndices });
 	instructions.push_back({ ior, 0, "ior" });
 	instructions.push_back({ irem, 0, "irem" });
-	instructions.push_back({ ior, 0, "ior" });
 	instructions.push_back({ ireturn, 0, "ireturn" });
+	instructions.push_back({ ishl , 0, "ishl" });
 	instructions.push_back({ ishr, 0, "ishr" });
-	instructions.push_back({ istore, 1, "istore", SignedBytePrinter });
+	instructions.push_back({ istore, 1, "istore", SignedBytePrinter }); // UNSIGNED BYTE LOCAL VARIABLE
+	instructions.push_back({ istore_0, 0, "istore_0"});
+	instructions.push_back({ istore_1, 0, "istore_1" });
+	instructions.push_back({ istore_2, 0, "istore_2" });
+	instructions.push_back({ istore_3, 0, "istore_3" });
 	instructions.push_back({ isub, 0, "isub" });
 	instructions.push_back({ iushr, 0, "iushr" });
 	instructions.push_back({ ixor, 0, "ixor" });
-	instructions.push_back({ jsr, 2, "jsr", ShortIndices });
-	instructions.push_back({ jsr_w, 0, "jsr_w", ShortIndices }); // TODO: Use wide printer
+	instructions.push_back({ jsr, 2, "jsr", ShortIndices }); // SIGNED 16 bit address
+	instructions.push_back({ jsr_w, 4, "jsr_w", ShortIndices }); // SIGNED 32 bit address
 	instructions.push_back({ l2d, 0, "l2d" });
 	instructions.push_back({ l2f, 0, "l2f" });
 	instructions.push_back({ l2i, 0, "l2i" });
@@ -464,10 +486,11 @@ ClassPrinter::ClassPrinter()
 	instructions.push_back({ laload, 0, "laload" });
 	instructions.push_back({ land, 0, "land", });
 	instructions.push_back({ lastore, 0, "lastore" });
+	instructions.push_back({ lcmp, 0, "lcmp" });
 	instructions.push_back({ lconst_0, 0, "lconst_0" });
 	instructions.push_back({ lconst_1, 0, "lconst_1" });
 	instructions.push_back({ i_ldiv, 0, "ldiv" });
-	instructions.push_back({ lload, 1, "lload", SignedBytePrinter });
+	instructions.push_back({ lload, 1, "lload", SignedBytePrinter }); // UNSIGNED BYTTE AND INDEX+1 UNSIGNED BYTE
 	instructions.push_back({ lload_0, 0, "lload_0" });
 	instructions.push_back({ lload_1, 0, "lload_1" });
 	instructions.push_back({ lload_2, 0, "lload_2" });
@@ -480,7 +503,7 @@ ClassPrinter::ClassPrinter()
 	instructions.push_back({ lreturn, 0, "lreturn" });
 	instructions.push_back({ lshl, 0, "lshl" });
 	instructions.push_back({ lshr, 0, "lshr" });
-	instructions.push_back({ lstore, 1, "lstore", SignedBytePrinter });
+	instructions.push_back({ lstore, 1, "lstore", SignedBytePrinter }); // INDEX AND INDEX+1 UNSIGNED BYTES
 	instructions.push_back({ lstore_0, 0, "lstore_0" });
 	instructions.push_back({ lstore_1, 0, "lstore_1" });
 	instructions.push_back({ lstore_2, 0, "lstore_2" });
@@ -499,7 +522,7 @@ ClassPrinter::ClassPrinter()
 	instructions.push_back({ ret, 1, "ret", SignedBytePrinter });
 	instructions.push_back({ saload, 0, "saload" });
 	instructions.push_back({ sastore, 0, "sastore" });
-	instructions.push_back({ sipush, 2, "sipush", ShortIndices });
+	instructions.push_back({ sipush, 2, "sipush", ShortIndices }); // SIGNED 16 bit value
 	instructions.push_back({ swap, 0, "swap" });
 	// TODO: Add custom printer for tableswitch
 	// TODO: Add custom printer for wide
