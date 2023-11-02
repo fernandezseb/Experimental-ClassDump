fe45fd6bcebb96b03e9b84b11005fd8bd28c227e
Sebastiaan Fernandez <fernandezsebastiaan@gmail.com>
2023-11-02 11:28:28 +0100

    Refactor header files and defines in Core.h

--- src/Core.h
+++ src/Core.h
@@ -2,22 +2,13 @@
 
 #define __STDC_FORMAT_MACROS
 
-#include <inttypes.h>
-
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
-#ifdef _DEBUG
-#ifndef DBG_NEW
-#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
-#define new DBG_NEW
-#endif
-#endif  // _DEBUG
-
 #ifdef _CPPRTTI
 #error "RTTI enabled"
 #endif
 
+#include <cstdio>
+#include <cstring>
+#include <cinttypes>
 
 #include <iostream>
 #include <algorithm>
@@ -26,6 +17,5 @@
 #include <string>
 #include <vector>
 #include <map>
-
 #include <sstream>
 #include <iterator>
