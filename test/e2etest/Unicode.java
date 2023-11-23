826117586df819e02550c3e9d07e8eaf3469c8b8
Sebastiaan Fernandez <fernandezsebastiaan@gmail.com>
2023-11-23 14:35:04 +0100

    Add Unicode test file

---
+++ test/e2etest/Unicode.java
@@ -0,0 +1,9 @@
+
+public class Unicode {
+    public static void main(String[] args) {
+        String twoByte = "\u00DF";
+        String threeByte = "\u6771";
+        String supplementary = "\uD840\uDC00";
+    }
+
+}
